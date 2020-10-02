1ed9bbe57220b9e9c48ab468568327a74250f460
Naina0509 <71021008+Naina0509@users.noreply.github.com>
2020-10-02 08:54:42 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,22 @@
+class Paper  {
+constructor(x,y,width,height){
+var options={
+    isStatic : false
+    restitution : 0.3
+    density : 1.2
+    friction : 0.5
+
+    this.body = Bodies.ellipse(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+}
+
+
+
+
+}
+
+
+
+
+}
